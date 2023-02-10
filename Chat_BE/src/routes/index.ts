7ce95399834fc5129aae1179607ccdc15159bf69
Vu Truong An <anvt@hybrid-technologies.vn>
2023-02-10 15:32:53 +0700

    Add file config nginx BE

--- Chat_BE/src/routes/index.ts
+++ Chat_BE/src/routes/index.ts
@@ -4,6 +4,6 @@ const router = express.Router();
 import authRouter from "./auth";
 
 router.use("/oauth", authRouter);
-router.use("/api/v1", apiRouter);
+// router.use("/api/v1", apiRouter);
 
 export default router;
